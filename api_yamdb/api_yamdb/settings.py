4a407e7515dda7d459e3d980ff1f8c00be55f592
Alexander Kolesov <4kolesov@gmail.com>
2022-08-08 19:31:33 +0300

    ReviewsConfig =)

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -20,7 +20,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'users.apps.UsersConfig',
-    'reviews.apps.AppConfig'
+    'reviews.apps.ReviewsConfig'
 ]
 
 AUTH_USER_MODEL = 'users.User'
