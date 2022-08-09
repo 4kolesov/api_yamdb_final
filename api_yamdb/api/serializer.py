f0317f6aceb2716b2a029dcb281ca3c85c487946
Рабадан Ибрагимов <raba@mail.ru>
2022-08-09 23:31:19 +0300

    Работаю над Title, добавил отображения рейтинга

--- api_yamdb/api/serializer.py
+++ api_yamdb/api/serializer.py
@@ -1,4 +1,6 @@
 from rest_framework import serializers
+import datetime
+from django.db.models import Sum
 
 from reviews.models import Category, Genre, Title
 
@@ -16,6 +18,16 @@ class GenreSerializer(serializers.ModelSerializer):
 
 
 class TitleSerializer(serializers.ModelSerializer):
+    year = serializers.DecimalField(
+        max_digits=4, decimal_places=0, max_value=datetime.date.today().year)
+    rating = serializers.SerializerMethodField()
     class Meta:
-        fields = ('id', 'name', 'year', 'description', 'genre', 'category')
+        fields = (
+            'id', 'name', 'year', 'rating', 'description', 'genre', 'category'
+        )
         model = Title
+
+    def get_rating(self, obj):
+        count = obj.reviews.count()
+        summ = obj.reviews.aggregate(Sum('score'))
+        return int(summ['score__sum']/count)
