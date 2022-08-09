31b62f1f027177de478681410c4e931fc8b1f5ba
Рабадан Ибрагимов <raba@mail.ru>
2022-08-09 20:56:15 +0300

    Работа над Категориями, добавил поиск. (остались уровни допуска, уже после готовой части users)

--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -1,4 +1,5 @@
 from rest_framework import viewsets
+from rest_framework.filters import SearchFilter
 from .viewsets import ListCreateDeleteViewSet
 from .serializer import CategorySerializer
 from  reviews.models import Category
@@ -7,3 +8,6 @@ from  reviews.models import Category
 class CategoryViewSet(ListCreateDeleteViewSet):
     serializer_class = CategorySerializer
     queryset = Category.objects.all()
+    lookup_field = 'slug'
+    filter_backends = (SearchFilter,)
+    search_fields = ('name',)
