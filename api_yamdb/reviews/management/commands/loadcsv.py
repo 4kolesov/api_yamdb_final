b78f5ac92a8ad5c007592b824e315ae4a5f51b37
Рабадан Ибрагимов <raba@mail.ru>
2022-08-14 22:13:10 +0300

    Модифицировал loadcsv

--- api_yamdb/reviews/management/commands/loadcsv.py
+++ api_yamdb/reviews/management/commands/loadcsv.py
@@ -1,14 +1,42 @@
 import csv
 
-from django.conf import settings
 from django.core.management.base import BaseCommand
+from django.conf import settings
 
-from reviews.models import Category, Genre, Title, Review, Comment
 from users.models import User
+from reviews.models import Category, Genre, Title, Review, Comment
 
 
 class Command(BaseCommand):
-    help = 'загружает данные в БД из csv.'
+    help = (f'Загружает данные в БД из csv.'
+            f'csv файлы должны находиться в дериктории: '
+            f'{settings.STATICFILES_DIRS[0]}')
+
+    def add_arguments(self, parser):
+        parser.add_argument(
+            '--clear_base',
+            action='store_true',
+            help = 'Очищает таблицы моделей перед заполнением.'
+        )
+        parser.add_argument(
+            '--only_err_msg',
+            action='store_true',
+            help = 'Выводит только основную информацию и сообщения об ошибках.'
+        )
+
+    def clear_tables(self, models, err_msg=False):
+        print('Начинается очистка таблиц.')
+        for model in models:
+            if not err_msg:
+                print(
+                    f'Удаление записей из таблицы модели: {model.__name__}'
+                )
+            try:
+                model.objects.all().delete()
+            except Exception as err:
+                print(f'Ошибка при попытке удалить записи из {model.__name__}:'
+                      f' {err}')
+        print('Очистка таблиц завершена.')
 
     def handle(self, *args, **options):
         CATEGORY = 'data/category.csv'
@@ -18,6 +46,7 @@ class Command(BaseCommand):
         REVIEW = 'data/review.csv'
         COMMENT = 'data/comments.csv'
         GENRE_TITLE = 'data/genre_title.csv'
+        MODELS = [Category, Genre, User, Title, Review, Comment]
         FILE_MODEL = {
             CATEGORY: Category,
             GENRE: Genre,
@@ -27,39 +56,49 @@ class Command(BaseCommand):
             COMMENT: Comment,
             GENRE_TITLE: Title
         }
+        if options['clear_base']:
+            self.clear_tables(MODELS, options['only_err_msg'])
+
         for url, model in FILE_MODEL.items():
             path = f'{settings.STATICFILES_DIRS[0]}{url}'
             print(f'Начинается загрузка из {url}')
-            with open(path, newline='', encoding='utf-8') as file:
-                csvfile = csv.DictReader(file, delimiter=',')
-                for row in csvfile:
-                    try:
-                        if url == TITLE:
-                            cat = Category.objects.get(id=row.pop('category'))
-                            model.objects.create(**row, category=cat)
-                        elif url == REVIEW:
-                            title = Title.objects.get(id=row.pop('title_id'))
-                            author = User.objects.get(id=row.pop('author'))
-                            model.objects.create(
-                                **row, title=title, author=author
-                            )
-                        elif url == COMMENT:
-                            review = Review.objects.get(
-                                id=row.pop('review_id')
-                            )
-                            author = User.objects.get(id=row.pop('author'))
-                            model.objects.create(
-                                **row, review=review, author=author
-                            )
-                        elif url == GENRE_TITLE:
-                            genre = Genre.objects.get(id=row.pop('genre_id'))
-                            title = Title.objects.get(id=row.pop('title_id'))
-                            title.genre.add(genre)
-                            title.save()
-                        else:
-                            model.objects.create(**row)
-                        print(f'Запись {row["id"]} добавлена')
-                    except Exception as err:
-                        print(f'Какая то ошибка...{err}')
-            print('почти готово...')
-        print('Теперь точно готово!')
+            try:
+                with open(path, newline='', encoding='utf-8') as file:
+                    csvfile = csv.DictReader(file, delimiter=',')
+                    for row in csvfile:
+                        try:
+                            if url == TITLE:
+                                cat = Category.objects.get(
+                                    id=row.pop('category'))
+                                model.objects.create(**row, category=cat)
+                            elif url == REVIEW:
+                                title = Title.objects.get(
+                                    id=row.pop('title_id'))
+                                author = User.objects.get(id=row.pop('author'))
+                                model.objects.create(
+                                    **row, title=title, author=author
+                                )
+                            elif url == COMMENT:
+                                review = Review.objects.get(
+                                    id=row.pop('review_id')
+                                )
+                                author = User.objects.get(id=row.pop('author'))
+                                model.objects.create(
+                                    **row, review=review, author=author
+                                )
+                            elif url == GENRE_TITLE:
+                                genre = Genre.objects.get(
+                                    id=row.pop('genre_id'))
+                                title = Title.objects.get(
+                                    id=row.pop('title_id'))
+                                title.genre.add(genre)
+                                title.save()
+                            else:
+                                model.objects.create(**row)
+                            if not options['only_err_msg']:
+                                print(f'Запись {row["id"]} добавлена')
+                        except Exception as err:
+                            print(f'Какая то ошибка...{err}')
+                print(f'Загрузка из {url} завершена.')
+            except FileNotFoundError as err:
+                print(f'Нет файла {url} в нужной дериктории: {err}')
