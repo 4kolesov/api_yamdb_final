269689e4632628cf41c099de44d01752d3107687
Рабадан Ибрагимов <raba@mail.ru>
2022-08-16 01:17:16 +0300

    Доработки коментов.

--- api_yamdb/api/serializer.py
+++ api_yamdb/api/serializer.py
@@ -81,10 +81,6 @@ class ReviewSerializer(serializers.ModelSerializer):
 
 
 class CommentSerializer(serializers.ModelSerializer):
-    review = serializers.SlugRelatedField(
-        slug_field='text',
-        read_only=True
-    )
     author = serializers.SlugRelatedField(
         slug_field='username',
         read_only=True
