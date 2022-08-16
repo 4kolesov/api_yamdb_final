50e4f89622c4148fbaffa8236303a05fa0bfffec
Рабадан Ибрагимов <raba@mail.ru>
2022-08-16 05:23:12 +0300

    мастер

--- api_yamdb/api/urls.py
+++ api_yamdb/api/urls.py
@@ -2,11 +2,8 @@ from django.urls import include, path
 from rest_framework.routers import SimpleRouter
 
 from .views import (CategoryViewSet, CommentViewSet, GenreViewSet,
-                    GetTokenView, ReviewViewSet, SignUpViewSet, TitleViewSet,
+                    ReviewViewSet, TitleViewSet,
                     UserViewSet)
-from .views import (CategoryViewSet, GenreViewSet,
-                    TitleViewSet, UserViewSet)
-# from .views import SignUpViewSet
 from .views import signup_user, get_token
 
 app_name = 'api'
@@ -15,7 +12,6 @@ router_v1 = SimpleRouter()
 router_v1.register('categories', CategoryViewSet, basename='categories')
 router_v1.register('genres', GenreViewSet, basename='genres')
 router_v1.register('titles', TitleViewSet, basename='titles')
-# router_v1.register('auth/signup', SignUpViewSet, basename='signup')
 router_v1.register('users', UserViewSet, basename='users')
 router_v1.register(
     r'titles/(?P<title_id>\d+)/reviews',
